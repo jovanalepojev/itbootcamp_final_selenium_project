f345916e23a80d7bee93f5a5dd9b637a5da11a27
Jovana Lepojev <lepojev.j@gmail.com>
2023-02-10 12:28:06 +0100

    Created Base Page

--- src/main/java/pages/BasePage.java
+++ src/main/java/pages/BasePage.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 public class BasePage {
     protected WebDriverWait wait;
     protected WebDriver driver;
+    protected String baseUrl = "https://vue-demo.daniel-avellaneda.com/";
 
     public BasePage(WebDriver driver, WebDriverWait wait) {
         this.driver = driver;
