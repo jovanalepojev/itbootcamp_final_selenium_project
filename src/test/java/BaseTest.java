cf0ed67fffaacfe73c82f55a03493313cfc6fa59
Jovana Lepojev <lepojev.j@gmail.com>
2023-02-13 10:28:55 +0100

    After Method update

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -1,15 +1,22 @@
 import helper.Helper;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.ITestResult;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import pages.*;
 
+import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.text.SimpleDateFormat;
 import java.time.Duration;
+import java.util.Date;
 
 public abstract class BaseTest {
     protected WebDriver driver;
@@ -42,14 +49,18 @@ public abstract class BaseTest {
     }
 
     @AfterMethod
-    public void afterMethod() {
-
+    public void afterMethod(ITestResult result) throws IOException {
+        if (result.getStatus() == ITestResult.FAILURE) {
+            File file = ((TakesScreenshot)driver).
+                    getScreenshotAs(OutputType.FILE);
+            String timestamp = new SimpleDateFormat("dd-MM-yyyy__hh-mm-ss").format(new Date());
+            Files.copy(file.toPath(), new
+            File("screenshots/" + result.getName() + " - " +
+                    timestamp + ".png").toPath());
+        }
     }
-
     @AfterClass
-    public void afterClass() throws InterruptedException, IOException {
-
-        new Helper().takeScreenshot(driver,  "relativePath");
+    public void afterClass() throws InterruptedException {
         Thread.sleep(2000);
         driver.quit();
     }
